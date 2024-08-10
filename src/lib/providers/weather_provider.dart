ea73667e605e46bc013502ce0a8e491ebd1bf15e
Tomas Westerlund <tomaswesterlund@gmail.com>
2024-08-10 14:02:07 -0600

    Update weather_provider.dart

--- src/lib/providers/weather_provider.dart
+++ src/lib/providers/weather_provider.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/weather_model.dart';
 
